027a73c1ba72c44241ef3d81767617ca2fe69dfa
Lonnie.Liang <lonnie.liang>
2022-05-03 15:50:20 +0800

    update BobbleSort.kt

--- app/src/main/java/com/liang/tind/leetcode/sort/BobbleSort.kt
+++ app/src/main/java/com/liang/tind/leetcode/sort/BobbleSort.kt
@@ -13,9 +13,9 @@ class BobbleSort : Sort() {
             println(arr.joinToString())
         }
 
-        fun bobbleSort(arr: IntArray) {
-            var flag = true
+        private fun bobbleSort(arr: IntArray) {
             for (i in 0 until arr.size - 1) {
+                var flag = true
                 for (j in 0 until arr.size - 1 - i) {
                     if (arr[j] > arr[j + 1]) {
                         swap(arr, j, j + 1)
